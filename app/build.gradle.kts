00a5335bef65bce8677fc9f96443ef49be7b77d1
Stefan Wärting <stefan@warting.se>
2024-10-26 15:52:41 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
                         // in which case the managed device will use a 32 bit image.
                         // Not applicable to arm64 machines.
                         require64Bit = false
+                        additionalOptions = ["-no-window", "-no-audio"]
                     }
                 )
             }
