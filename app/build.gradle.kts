725273a46fbd1351fd25343d9d9335058c2681f9
Stefan Wärting <stefan@warting.se>
2024-10-26 09:56:45 +0200

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+import com.android.build.gradle.internal.dsl.ManagedVirtualDevice
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "se.warting.gradlemanageddevices"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "se.warting.gradlemanageddevices"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    testOptions {
+        animationsDisabled = true
+        managedDevices {
+            devices {
+                add(
+                    ManagedVirtualDevice("pixel2api30").apply {
+                        // Use device profiles you typically see in Android Studio.
+                        device = "Pixel 2"
+                        // Use only API levels 27 and higher.
+                        apiLevel = 30
+                        // To include Google services, use "google".
+                        systemImageSource = "google"
+                        //systemImageSource = "aosp-atd"
+                        // Whether the image must be a 64 bit image. Defaults to false,
+                        // in which case the managed device will use a 32 bit image.
+                        // Not applicable to arm64 machines.
+                        require64Bit = false
+                    }
+                )
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
